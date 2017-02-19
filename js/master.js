04b5258baf95b7907ec5c6d0a2b1160ab9354b2a
Dennis Konieczek <konieczekdennis@gmail.com>
2017-02-19 16:06:06 -0500

    particles added, modified colors, general UI

--- js/master.js
+++ js/master.js
@@ -1,4 +1,8 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    particlesJS.load('particles-js', 'assets/particles.json', function () {
+        console.log('callback - particles.js config loaded');
+    });
 
 });
+
