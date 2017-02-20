f8c4b5725932c576dcc7bbfd199b2b4ff40036c7
Dennis Konieczek <konieczekdennis@gmail.com>
2017-02-19 19:01:08 -0500

    added animated scrolling and fixed express logo

--- js/master.js
+++ js/master.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
-    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
     particlesJS.load('particles-js', 'assets/particles.json', function () {
         console.log('callback - particles.js config loaded');
     });
-
 });
 
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1000);
+});
