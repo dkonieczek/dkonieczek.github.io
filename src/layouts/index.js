45c82163e9dc3cea4b30111594125bb524059f3b
Dennis Konieczek <konieczekdennis@gmail.com>
2017-09-08 14:25:35 -0400

    header implemented

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,64 +1,52 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
 
-import './index.css'
+import './index.scss';
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
+  <div className="header-wrapper">
+    <div className="header">
+      <h1>
+        <Link to="/">Dennis Konieczek</Link>
       </h1>
+      <div className="nav-item">
+        <Link to="/about">About</Link>
+      </div>
+      <div className="nav-item">
+        <Link to="/portfolio">Portfolio</Link>
+      </div>
+      <div className="nav-item">
+        <Link to="/contact">Contact</Link>
+      </div>
     </div>
   </div>
-)
+);
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Dennis Konieczek"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        {
+          name: 'description',
+          content:
+            'Dennis Konieczek - Full stack web developer based in Toronto, Canada.,'
+        },
+        {
+          name: 'keywords',
+          content: 'Dennis Konieczek,web,developer,portfolio'
+        }
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <div className="page-wrapper">{children()}</div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
