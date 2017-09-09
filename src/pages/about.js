604887fd6ba4fe819cf41c4344a5ac73a11d9fb5
Dennis Konieczek <konieczekdennis@gmail.com>
2017-09-09 16:30:55 -0400

    skills section implemented

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import Link from 'gatsby-link';
 
 const AboutPage = () => (
   <div className="about">
+    <h2>About Me</h2>
     <p className="bio">
       Hey there! My name is Dennis Konieczek, I am a full stack web developer
       based in Toronto, Canada. I love all things tech, however I’ve focused my
@@ -10,26 +11,99 @@ const AboutPage = () => (
       have worked on multiple exciting projects which you can check out here on
       my personal website.
     </p>
+    <h3>Experience</h3>
     <ul>
       <li>
-        <p>07/2016 - 07/2017</p>
+        <p className="date">07/2016 - Present</p>
         <p>
           Technical Support &amp; Software Developer
           <span>
-            Responsibilities: HTML, CSS, JavaScript, NodeJS, jQuery, PHP
+            Tools: HTML, CSS, JavaScript, NodeJS, jQuery, PHP
+            <br />
+            <br />
+            Developed solutions for integrating onboarding clients from
+            ecommerce platforms such as Magento, Volusion, Bigcommerce, Yahoo,
+            Shopify, 3DCart and NetSuite.
+            <br />
+            <br />
+            Developed automated end to end test cases.
+            <br />
+            <br />
+            Provided technical email &amp; phone support.
+            <br />
+            <br />
+            Participated and provide opinion in daily standups and weekly sprint
+            meetings as part of the scrum agile methodology.
           </span>
         </p>
       </li>
       <li>
-        <p>07/2014 - 07/2015</p>
+        <p className="date">07/2014 - 07/2015</p>
         <p>
           Technical Support Specialist
           <span>
-            Responsibilities: Hardware, application and network support
+            Provided hardware, application and network specific support.
+            <br />
+            <br />
+            Managed physical hardware assets
+            <br />
+            <br />
+            Deployment of Cisco VoIP hardware
           </span>
         </p>
       </li>
     </ul>
+    <h3>Skills</h3>
+    <div className="skills">
+      <div className="skill">
+        <img src="http://via.placeholder.com/100x100" />
+        <p className="heading">Design</p>
+        <p className="desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus
+          tincidunt laoreet.
+        </p>
+      </div>
+      <div className="skill">
+        <img src="http://via.placeholder.com/100x100" />
+        <p className="heading">Design</p>
+        <p className="desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus
+          tincidunt laoreet.
+        </p>
+      </div>
+      <div className="skill">
+        <img src="http://via.placeholder.com/100x100" />
+        <p className="heading">Design</p>
+        <p className="desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus
+          tincidunt laoreet.
+        </p>
+      </div>
+      <div className="skill">
+        <img src="http://via.placeholder.com/100x100" />
+        <p className="heading">Design</p>
+        <p className="desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus
+          tincidunt laoreet.
+        </p>
+      </div>
+      <div className="skill">
+        <img src="http://via.placeholder.com/100x100" />
+        <p className="heading">Design</p>
+        <p className="desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus
+          tincidunt laoreet.
+        </p>
+      </div>
+      <div className="skill">
+        <img src="http://via.placeholder.com/100x100" />
+        <p className="heading">Design</p>
+        <p className="desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus
+          tincidunt laoreet.
+        </p>
+      </div>
+    </div>
   </div>
 );
 
