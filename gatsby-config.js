3fe5c0bf5458d8b381be331294e244c10edce904
Dennis Konieczek <konieczekdennis@gmail.com>
2017-09-16 18:47:30 -0400

    deply

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     title: `Gatsby Default Starter`
   },
   plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`],
-  pathPrefix: `/dkonieczek.com/public`
+  pathPrefix: `/public`
 };
