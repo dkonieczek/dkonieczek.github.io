b3809b76bbb7159823dd989311dcdd59f31a023d
Dennis Konieczek <konieczekdennis@gmail.com>
2018-03-28 17:23:38 -0400

    added GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,21 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`]
+  plugins: [
+    `gatsby-plugin-react-helmet`, 
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-116573741-1",
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    }
+  ]
   //pathPrefix: `/public`
 };
